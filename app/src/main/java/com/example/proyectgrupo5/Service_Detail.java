68e4bd0fecc38415151a95dce00b6ddce5c374d2
EFZR <emerson.zapatarivas@gmail.com>
2022-03-28 22:50:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/proyectgrupo5/Service_Detail.java
@@ -0,0 +1,14 @@
+package com.example.proyectgrupo5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Service_Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_detail);
+    }
+}
